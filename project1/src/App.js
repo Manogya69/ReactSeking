68d17f5eb25cbe355e5a775c72a8fce8367940c8
manogya69 <manogya57@gmail.com>
2023-04-01 21:11:02 +0545

    Styled the todo card and buttons

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div>
-      <h1> Project Started</h1>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
