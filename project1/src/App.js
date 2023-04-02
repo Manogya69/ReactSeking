e6588f76eecff63f77fc2da99dec541e4adf1695
manogya69 <manogya57@gmail.com>
2023-04-02 19:53:54 +0545

    Understanding Props

--- project1/src/App.js
+++ project1/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learning React" />
+      <Todo text="Creating a React Project" />
+      <Todo text="Learning Python" />
     </div>
   );
 }
