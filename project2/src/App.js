991dbf0741e896ea471c5395d1041d26f1519951
manogya69 <manogya57@gmail.com>
2023-04-05 19:57:47 +0545

    added Main Navigation

--- project2/src/App.js
+++ project2/src/App.js
@@ -3,21 +3,25 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <Route
-        path="/"
-        exact>
-        <AllMeetupsPage />
-      </Route>
-      {/* <Route path="/new-meetup">
-        <NewMeetupPage />
-      </Route>
-      <Route path="/favroites">
-        <FavoritesPage />
-      </Route>  */}
+      <MainNavigation />
+      <Switch>
+        <Route
+          path="/"
+          exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favroites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
