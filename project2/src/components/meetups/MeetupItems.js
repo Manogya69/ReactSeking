3f9564e423e9830d4261db6160b07318c13dc7c5
manogya69 <manogya57@gmail.com>
2023-04-06 20:21:00 +0545

    Added props to title and alt and added className to div instead of button

--- project2/src/components/meetups/MeetupItems.js
+++ project2/src/components/meetups/MeetupItems.js
@@ -1,11 +1,12 @@
 import classes from "./MeetupItem.module.css";
+
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
       <div className={classes.image}>
         <img
-          src="{props.image}"
-          alt=""
+          src={props.image}
+          alt={props.title}
         />
       </div>
       <div className={classes.content}>
@@ -13,8 +14,8 @@ function MeetupItem(props) {
         <address>{props.address}</address>
         <p>{props.description}</p>
       </div>
-      <div>
-        <button className={classes.actions}>To Favorites</button>
+      <div className={classes.actions}>
+        <button>To Favorites</button>
       </div>
     </li>
   );
