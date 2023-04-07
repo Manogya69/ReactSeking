0869cb48061792882599e8eda9e407e0bf3c7d17
manogya69 <manogya57@gmail.com>
2023-04-07 21:18:41 +0545

    Created a new Meetup Form and its CSS

---
+++ project2/src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,46 @@
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}></form>
+      <div className={classes.control}>
+        <label htmlFor="title">Meetup Title</label>
+        <input
+          type="text"
+          required
+          id="title"
+        />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="image">Meetup Image</label>
+        <input
+          type="url"
+          required
+          id="image"
+        />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="address">Meetup Address</label>
+        <input
+          type="text"
+          required
+          id="address"
+        />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="description">Meetup Description</label>
+        <textarea
+          id="description"
+          rows="5"
+          required></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button>Add Meetup</button>
+      </div>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
