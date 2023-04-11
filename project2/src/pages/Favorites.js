335d4a73d50fb22ab20aa248838c2426137b7c84
manogya69 <manogya57@gmail.com>
2023-04-11 20:27:45 +0545

    added a paragraph to show when there is no favorites

--- project2/src/pages/Favorites.js
+++ project2/src/pages/Favorites.js
@@ -5,10 +5,17 @@ import MeetupList from "../components/meetups/MeetupList";
 function FavoritesPage() {
   const favoritesCtx = useContext(FavoritesContext);
 
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You have no Favorites at the moment! Start adding some!</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
   return (
     <section>
       <h1>My Favorites</h1>
-      <MeetupList meetups={favoritesCtx.favorites} />
+      {content}
     </section>
   );
 }
