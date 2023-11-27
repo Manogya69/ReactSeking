22287d8a35edea0e866affc008841852b6cf3c1a
Manogya <manogya57@gmail.com>
2023-11-28 00:37:07 +0545

    using props to show dynamic data

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,11 +1,47 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return(
+  const expenses = [
+    {
+      id: "e1",
+      title: "Junk Food",
+      amount: 1000,
+      date: new Date(2023, 11, 27),
+    },
+    {
+      id: "e2",
+      title: "Chiya Churot",
+      amount: 500,
+      date: new Date(2023, 11, 25),
+    },
+    { id: "e3", title: "Weed", amount: 1500, date: new Date(2023, 11, 23) },
+    { id: "e4", title: "Gadi Vada", amount: 500, date: new Date(2023, 11, 22) },
+  ];
+
+  return (
     <div>
-        <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
-  )
+  );
 }
 
 export default App;
