c611aec4046c164577681d2e014a827f01d0b0ec
Manogya <manogya57@gmail.com>
2024-03-15 13:54:10 +0545

    used CartProvider to dynamically update the number of cart icon

--- 5thProjectFoodOrderingApp/src/components/Layout/HeaderCartButton.js
+++ 5thProjectFoodOrderingApp/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,25 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import { useContext } from "react";
 
-const HeaderCartButton = props => {
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            69
-        </span>
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-}
+  );
+};
 
 export default HeaderCartButton;
