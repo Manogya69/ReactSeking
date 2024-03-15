485d37770a40b19a1acd7a73068e08c48afd765c
Manogya <manogya57@gmail.com>
2024-03-15 13:52:12 +0545

    wraped components with CartProvider component

--- 5thProjectFoodOrderingApp/src/App.js
+++ 5thProjectFoodOrderingApp/src/App.js
@@ -1,29 +1,28 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-const showCartHandler = () => {
-  setCartIsShown(true);
-}
-
-const hideCardHandler = () =>{
-  setCartIsShown(false);
-}
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCardHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
